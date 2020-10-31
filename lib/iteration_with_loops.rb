362d725560de9e7781d8d75d97a0dfd96c491533
Winston Chen <winstonchen.wm@gmail.com>
2020-10-31 02:53:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index].even?
-    p src[row_index][element_index]
+    if src[row_index][element_index].even? 
+      p src[row_index][element_index]
     element_index += 1
   end
   row_index += 1
